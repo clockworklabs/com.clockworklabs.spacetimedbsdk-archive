851790f833856381ededd53ce8feedd9e7645fd2
Derek Brinkmann <dbrinkmann@citadelstudios.net>
2023-07-05 16:11:57 -0400

    v0.5.0

---
+++ Scripts/ReducerExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SpacetimeDB
+{
+    public class ReducerCallbackAttribute : Attribute
+    {
+        public string FunctionName { get; set; }
+    }
+
+    public class DeserializeEventAttribute : Attribute
+    {
+        public string FunctionName { get; set; }
+    }
+}

34c155efa6cd38229e4bc2c2a6cffcc769c9effb
Dylan Hunt <dylan@imperium42.com>
2024-04-30 10:49:04 +0800

    fix: Ambiguous log

--- Editor/Common/SpacetimeWindow.cs
+++ Editor/Common/SpacetimeWindow.cs
@@ -93,7 +93,7 @@ namespace SpacetimeDB.Editor
         public static void CopyToClipboard(string str)
         {
             GUIUtility.systemCopyBuffer = str;
-            Debug.Log($"Copied to clipboard: {str}");
+            UnityEngine.Debug.Log($"Copied to clipboard: {str}");
         }
 
         public static string NormalizePath(string pathToProjFile) =>

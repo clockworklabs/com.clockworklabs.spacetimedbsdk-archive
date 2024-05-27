7be68d243bea096a6cef7abe6f1fc031eafb0b18
John Detter <no-reply@boppygames.gg>
2024-05-27 13:06:46 -0500

    Initial script

---
+++ tools/sync-with-csharp-sdk.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+
+include fix-meta-files.bash
+
+STDB_CSHARP="../../spacetimedb-csharp-sdk"
+
+if ! [ -d "../../spacetimedb-csharp-sdk" ] ; then
+    echo "Please clone the spacetimedb-csharp-sdk as a directory sibiling of the UnitySDK repo."
+    echo "git clone https://github.com/clockworklabs/spacetimedb-csharp-sdk.git"
+    exit 1
+fi
+
+if ! which rsync > /dev/null ; then
+    echo "This script requires rsync, which may be installed through brew on macOS:"
+    echo ""
+    echo "  brew install rsync"
+    echo ""
+    echo "or through your package manager in Linux."
+fi
+
+rsync -a $STDB_CSHARP/src/ ./Scripts/
+fix_meta_files
